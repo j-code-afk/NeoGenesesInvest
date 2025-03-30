ad159b57454faf606bfc7d9767e442ceccecdc0b
j-code-afk <joaopsoares015@gmail.com>
2025-03-30 14:25:44 -0300

    Update main.js

--- Front-end/src/main.js
+++ Front-end/src/main.js
@@ -2,3 +2,23 @@ function toggleSettings() {
     const panel = document.getElementById('settingsPanel');
     panel.classList.toggle('open');
 }
+
+const checkboxDarkmode = document.getElementById("toggleCheckbox-darkmode");
+
+
+//vê se o darkmode está ativado no LocalStorage
+if (localStorage.getItem("darkmode") === "enabled") {
+    document.body.classList.add("darkmode");
+    checkboxDarkmode.checked = true;
+}
+
+//Evento para alternar e salvar preferência
+checkboxDarkmode.addEventListener("change", function() {
+    if (this.checked) {
+        document.body.classList.add("darkmode");
+        localStorage.setItem("darkmode", "enabled");
+    } else {
+        document.body.classList.remove("darkmode");
+        localStorage.setItem("darkmode", "disabled");
+    }
+});
